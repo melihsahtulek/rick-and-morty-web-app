cc68c0edb38d7ef369f479df5ecce74576ded115
melihsahtulek <melihsahtulek@gmail.com>
2021-12-21 01:41:10 +0300

    redesign

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
 window.addEventListener("load", () => {
   const characters = document.querySelector("#characters");
   let currentPage = 1;
-  const pagination = document.querySelector("#paginationID");
 
   const getCharacters = async () => {
     const response = await fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}`);
     const json = await response.json();
-    return json;
+    writeCharacters(json.results);
   };
 
-  getCharacters().then((json) => {
-    let { results, info } = json;
-    console.log(info);
-    for (let i = currentPage; i <= 5; i++) {
-      console.log(i);
-    }
+  const writeCharacters = (results) => {
+    characters.innerHTML = null;
     results.forEach((character) => {
       characters.innerHTML += `
         <a href="#" class="card">
@@ -27,5 +22,7 @@ window.addEventListener("load", () => {
         </a>
       `;
     });
-  });
+  };
+
+  getCharacters();
 });
